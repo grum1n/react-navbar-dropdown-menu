8c8862370ee7bc5e085ab69b2234b5580d3cafc3
grum1n <grumin.el.pastas@gmail.com>
2021-10-13 09:33:47 +0300

    added function Navbar to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,4 +4,14 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import Dropdown from './Dropdown';
 
-
+function Navbar () {
+    return (
+        <>
+            <nav className='navbar'>
+                <Link to='/' className='navbar-logo'>
+                    Grumin
+                </Link>
+            </nav>
+        </>
+    )
+}
