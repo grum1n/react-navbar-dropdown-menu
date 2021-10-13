1bac81e6b7ff6c79d7662866df32ab7a209cb1ca
grum1n <grumin.el.pastas@gmail.com>
2021-10-13 09:48:54 +0300

    added menu icon to Navbar function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,19 @@ import './Navbar.css';
 import Dropdown from './Dropdown';
 
 function Navbar () {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
     return (
         <>
             <nav className='navbar'>
                 <Link to='/' className='navbar-logo'>
                     Grumin
                 </Link>
+                <div className='manu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
             </nav>
         </>
     )
