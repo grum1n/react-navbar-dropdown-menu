a5d11dc95630ddcd0af0e0c7e484fb13322c6e46
grum1n <grumin.el.pastas@gmail.com>
2021-10-13 10:27:26 +0300

    added Navbar links to Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,11 @@ import './Navbar.css';
 import Dropdown from './Dropdown';
 
 function Navbar () {
-    const [click, setClick] = useState(false)
+    const [click, setClick] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
 
-    const handleClick = () => setClick(!click)
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <>
@@ -18,6 +20,30 @@ function Navbar () {
                 <div className='manu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services <i className='fas fa-caret-down' />
+                        </Link>
+                        {dropdown && <Dropdown /> }
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                            Contact Us
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
+                <Button />
             </nav>
         </>
     )
